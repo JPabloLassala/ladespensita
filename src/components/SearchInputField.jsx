2d5f51453312ad531b5abd5e5267d17b388d27ea
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-04-29 11:52:39 -0300

    Add reset and filter checkbox

--- src/components/SearchInputField.jsx
+++ src/components/SearchInputField.jsx
@@ -1,11 +1,31 @@
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRef } from "react";
 
 export function SearchInputField({ placeholder, label, id, onChange }) {
+  const enableInputRef = useRef();
+  const inputRef = useRef();
+  function handleOnChange(event) {
+    if (event.target.value && !enableInputRef.current.checked) {
+      enableInputRef.current.checked = true;
+    }
+    onChange(event);
+  }
+  function handleClearInput() {
+    inputRef.current.value = "";
+    enableInputRef.current.checked = false;
+  }
+
   return (
     <div className="flex w-1/5 flex-col">
       <div className="mb-1 flex flex-row items-center">
-        <input id={`enable${id}`} name={`enable${id}`} type="checkbox" className="mr-1" />
+        <input
+          ref={enableInputRef}
+          id={`enable${id}`}
+          name={`enable${id}`}
+          type="checkbox"
+          className="mr-1"
+        />
         <label htmlFor={`enable${id}`} className=" font-semibold text-slate-700">
           {label}
         </label>
@@ -16,9 +36,15 @@ export function SearchInputField({ placeholder, label, id, onChange }) {
           type="text"
           id={id}
           name={id}
+          ref={inputRef}
           placeholder={placeholder}
-          onChange={onChange}
+          onChange={handleOnChange}
           className="ml-2 w-full bg-transparent text-sm text-slate-800 focus:outline-none"
+        />
+        <FontAwesomeIcon
+          icon={faXmark}
+          className="text-slate-500 hover:cursor-pointer"
+          onClick={handleClearInput}
         />
       </div>
     </div>
