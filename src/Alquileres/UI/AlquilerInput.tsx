43bbfdd870ae4eac1b6b2ac20db24c51cf9f7e3a
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-07-29 17:43:58 -0300

    Move Datepicker to another component

--- src/Alquileres/UI/AlquilerInput.tsx
+++ src/Alquileres/UI/AlquilerInput.tsx
@@ -1,5 +1,4 @@
 import { convertToCamelCase } from "@utils";
-import { useRef } from "react";
 
 export function AlquilerInput({
   titulo,
@@ -18,14 +17,6 @@ export function AlquilerInput({
   const flexOrientation = !row ? "flex-col" : "flex-row items-center mb-2";
   const labelRowClasses = row && "mr-2";
   const boldClass = bold && "font-semibold";
-  const dateInputRef = useRef<HTMLInputElement>(null);
-
-  function handleOpenDatePicker() {
-    dateInputRef.current?.showPicker();
-  }
-  function handleChangeToInput(e: React.ChangeEvent<HTMLInputElement>) {
-    onChange(e.target.value);
-  }
 
   return (
     <div className={`flex ${flexOrientation} w-1/2 px-1`}>
@@ -36,14 +27,7 @@ export function AlquilerInput({
         id={id}
         className="border shadow p-2 rounded"
         defaultValue={value}
-        onClick={handleOpenDatePicker}
         onChange={(e) => onChange(e.target.value)}
-      ></input>
-      <input
-        type="datetime-local"
-        className="h-0 overflow-hidden"
-        ref={dateInputRef}
-        onChange={handleChangeToInput}
       ></input>
     </div>
   );
