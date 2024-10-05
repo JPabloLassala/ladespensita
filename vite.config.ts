f128f9fe8b6c4146917afabe4fd804d7f4dc0c8e
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-10-05 18:32:35 -0300

    Redo path alias

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@stores": path.resolve(__dirname, "src/stores/"),
-      "@hooks": path.resolve(__dirname, "src/hooks/"),
-      "@schemas": path.resolve(__dirname, "src/schemas/"),
-      "@Productos": path.resolve(__dirname, "src/Productos/"),
-      "@Alquileres": path.resolve(__dirname, "src/Alquileres/"),
-      "@Shared": path.resolve(__dirname, "src/Shared/"),
-      "@utils": path.resolve(__dirname, "src/utils/"),
+      "@": path.resolve(__dirname, "src/"),
     },
   },
 });
