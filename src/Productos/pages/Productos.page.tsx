32c0a5389699b590982920255a2d40d318aa3142
Juan Pablo Lassala <jpablolassala@gmail.com>
2025-05-03 23:20:32 -0300

    Implement handleCreate and handleUpdate functions for product management in ProductosPage

--- src/Productos/pages/Productos.page.tsx
+++ src/Productos/pages/Productos.page.tsx
@@ -5,9 +5,11 @@ import { FilterContextProvider } from "@/Common";
 import { FilterProducts, ProductosListContainer, ProductosPageContainer } from "../components";
 import { useProductoRepository } from "../repository";
 import { CreateNewProducto } from "../components/CreateNewProducto";
+import { ProductoEntityCreate, ProductoEntityUpdate } from "../entities";
+import { FileWithPath } from "@mantine/dropzone";
 
 export function ProductosPage() {
-  const { productos, setProductos } = useContext(ProductosContext)!;
+  const { productos, setProductos, updateProducto } = useContext(ProductosContext)!;
   const { data, sendList, sendCreate, sendUpdate } = useProductoRepository(productos);
 
   useEffect(() => {
@@ -19,19 +21,60 @@ export function ProductosPage() {
     sendList();
   }, []);
 
+  const handleUpdate = (producto: ProductoEntityUpdate, id: number) => {
+    const formData = new FormData();
+    formData.append("file", producto.file as FileWithPath);
+    formData.append("nombre", producto.nombre);
+    formData.append("unidadesMetroLineal", producto.unidadesMetroLineal.toString());
+    formData.append("totales", producto.totales.toString());
+    formData.append("altura", producto.altura?.toString() || "");
+    formData.append("diametro", producto.diametro?.toString() || "");
+    formData.append("ancho", producto.ancho?.toString() || "");
+    formData.append("profundidad", producto.profundidad?.toString() || "");
+    formData.append("valorUnitarioGarantia", producto.valorUnitarioGarantia.toString());
+    formData.append("valorUnitarioAlquiler", producto.valorUnitarioAlquiler.toString());
+    formData.append("valorx1", producto.valorx1.toString());
+    formData.append("valorx3", producto.valorx3.toString());
+    formData.append("valorx6", producto.valorx6.toString());
+    formData.append("valorx12", producto.valorx12.toString());
+
+    sendUpdate(formData, id);
+    updateProducto(producto);
+  };
+
+  const handleCreate = (producto: ProductoEntityCreate) => {
+    const formData = new FormData();
+    formData.append("file", producto.file as FileWithPath);
+    formData.append("nombre", producto.nombre);
+    formData.append("unidadesMetroLineal", producto.unidadesMetroLineal.toString());
+    formData.append("totales", producto.totales.toString());
+    formData.append("altura", producto.altura?.toString() || "");
+    formData.append("diametro", producto.diametro?.toString() || "");
+    formData.append("ancho", producto.ancho?.toString() || "");
+    formData.append("profundidad", producto.profundidad?.toString() || "");
+    formData.append("valorUnitarioGarantia", producto.valorUnitarioGarantia.toString());
+    formData.append("valorUnitarioAlquiler", producto.valorUnitarioAlquiler.toString());
+    formData.append("valorx1", producto.valorx1.toString());
+    formData.append("valorx3", producto.valorx3.toString());
+    formData.append("valorx6", producto.valorx6.toString());
+    formData.append("valorx12", producto.valorx12.toString());
+
+    sendCreate(formData);
+  };
+
   return (
     <FilterContextProvider>
       <ProductosPageContainer>
         <FilterProducts />
         <ProductosListContainer>
-          <CreateNewProducto onCreate={sendCreate} />
+          <CreateNewProducto onCreate={handleCreate} />
           {productos.map((p) => (
             <ProductoCard
               key={p.id}
               producto={p}
               dimmed={false}
               disabled={false}
-              onUpdate={sendUpdate}
+              onUpdate={handleUpdate}
             />
           ))}
         </ProductosListContainer>
