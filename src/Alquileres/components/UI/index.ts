5250d595fcdf6affd2139099f7a1bcbcde47e20c
Juan Pablo Lassala <juan.pablo@kinema.com>
2025-10-22 16:11:51 -0300

    Remove alquiler list container

--- src/Alquileres/components/UI/index.ts
+++ src/Alquileres/components/UI/index.ts
@@ -1,6 +1,4 @@
-export * from "./AlquilerListContainer";
 export * from "./AlquilerListEntry";
 export * from "./AlquilerNoneSelected";
 export * from "./AlquilerProductosScrollContainer";
-export * from "./NewAlquilerListEntry";
 export * from "./AlquilerDetailsContainer";
