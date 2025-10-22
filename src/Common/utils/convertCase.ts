6dc5e30d59aa26c7b295a3624f18d2c1aa8d5ac4
Juan Pablo Lassala <juan.pablo@kinema.com>
2025-10-22 14:52:55 -0300

    Remove unused file

--- src/Common/utils/convertCase.ts
+++
@@ -1,9 +0,0 @@
-export const convertToCamelCase = (text: string) => {
-  return text
-    .split(" ")
-    .map((word, index) => {
-      if (index === 0) return word.toLowerCase();
-      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-    })
-    .join("");
-};
