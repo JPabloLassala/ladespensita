b71199d702f9ae6814ed1287ad7b1699db0a0c4b
JPabloLassala <jpablolassala@gmail.com>
2025-10-31 17:32:55 -0300

    Center NoAlquilerSelected

--- src/Alquileres/components/UI/AlquilerNoneSelected.tsx
+++ src/Alquileres/components/UI/AlquilerNoneSelected.tsx
@@ -2,7 +2,7 @@ import { Flex, Text } from "@mantine/core";
 
 export function AlquilerNoneSelected() {
   return (
-    <Flex align="center" justify="center" w="100%">
+    <Flex align="center" justify="center" w="100%" h="100%">
       <Text>No hay alquiler seleccionado</Text>
     </Flex>
   );
