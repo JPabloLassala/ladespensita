792c2fbaa58301a8ab7dfd962c01351d015241c1
JPabloLassala <jpablolassala@gmail.com>
2025-10-31 17:32:53 -0300

    Refactor UploadFile component to support ProductoImage and improve file preview logic

--- src/Productos/components/UploadFile.tsx
+++ src/Productos/components/UploadFile.tsx
@@ -3,23 +3,30 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button, Group, Image, Stack, Text, useMantineTheme } from "@mantine/core";
 import { Dropzone, FileWithPath, IMAGE_MIME_TYPE } from "@mantine/dropzone";
 import classes from "../Css/Dropzone.module.css";
+import { ProductoImage } from "../entities";
 
 export const UploadFile = ({
   file,
   onSetFile,
+  image,
 }: {
   file: File | undefined;
   onSetFile: (file: File) => void;
+  image?: ProductoImage;
 }) => {
   const theme = useMantineTheme();
 
   const preview = () => {
-    if (!file) return null;
-    const imageUrl = URL.createObjectURL(file);
+    if (!file && !image) return null;
 
-    return (
-      <Image fit="contain" h={200} src={imageUrl} onLoad={() => URL.revokeObjectURL(imageUrl)} />
-    );
+    let url: string = "";
+    if (image) {
+      url = image.url;
+    } else {
+      url = URL.createObjectURL(file as File);
+    }
+
+    return <Image fit="contain" h={200} src={url} onLoad={() => URL.revokeObjectURL(url)} />;
   };
 
   const handleDrop = (acceptedFiles: FileWithPath[]) => {
@@ -48,8 +55,8 @@ export const UploadFile = ({
             <FontAwesomeIcon icon={faX} size="2x" />
           </Dropzone.Reject>
           <Dropzone.Idle>
-            {file && preview()}
-            {!file && (
+            {(file || image) && preview()}
+            {!file && !image && (
               <Stack justify="center" align="center" h="100%" miw="100%">
                 <FontAwesomeIcon icon={faUpload} size="2x" color={theme.colors.blue[4]} />
                 <Text size="xl" inline>
