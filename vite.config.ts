0a179407efb9da0ef7bdf7d83d77f8bb54373129
JPabloLassala <jpablolassala@gmail.com>
2026-02-12 10:52:39 -0300

    Add compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({ algorithm: "brotliCompress" }),
+    compression({ algorithm: "gzip" }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src/"),
